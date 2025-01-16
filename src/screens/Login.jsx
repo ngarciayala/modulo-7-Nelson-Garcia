fd5488ca83efbb516be662a3cb5edd97ebfc8cac
Nelson <ngarciayala@gmail.com>
2025-01-16 19:46:55 -0400

    Clase 5

---
+++ src/screens/Login.jsx
@@ -0,0 +1,11 @@
+// import FormLogin from '../Components/Forms/FormLogin.jsx';
+// import FormLoginWithHook from "../Components/Forms/FormLoginWithHook.jsx";
+import FormLoginWithMotion from "../Components/Forms/FormLoginWithMotion.jsx";
+const Login = () => {
+    return (
+        <>
+            <FormLoginWithMotion titleForm="Welcome to FullStack program" />
+        </>
+    );
+};
+export default Login;
--- src/store/features/product/productSlice.js
+++ src/store/features/product/productSlice.js
@@ -17,16 +17,12 @@ export const productSlice = createSlice({
   initialState,
   reducers: {
     setinitialValue: (state, payload) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
       state.initialValue = payload
     },    
   },
 })
 
-// Action creators are generated for each case reducer function
+
 export const { setinitialValue } = productSlice.actions
 
 export default productSlice.reducer
