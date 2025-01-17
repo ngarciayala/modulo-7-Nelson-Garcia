76f1c631da463b49e7ec2c9cc3783aafd3f9e5d9
Nelson <ngarciayala@gmail.com>
2025-01-16 20:36:08 -0400

    GitHub

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/modulo-7-Nelson-Garcia/">
         <App />
       </BrowserRouter>    
     </Provider>    
